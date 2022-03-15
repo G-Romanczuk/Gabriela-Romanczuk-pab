4cdccf768fe0625be6e596098eb7d4a241c7374b
G-Romanczuk <gxbiromanczuk@gmail.com>
2022-03-15 19:19:38 +0100

    dodawanie

--- lab1/index.ts
+++ lab1/index.ts
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    res.send('Hello ')
 })
+app.get('/add/:num1/:num2', function (req, res) {
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    var result : number = num1 + num2;
+    res.send(num1 + ' + ' + num2 + ' = ' + result);
+});
 app.listen(3000)
