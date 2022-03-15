709f548b7165f5c0467480daf59beae1b3ce1745
G-Romanczuk <gxbiromanczuk@gmail.com>
2022-03-15 19:22:38 +0100

    kalkulatorek

--- lab1/index.js
+++ lab1/index.js
@@ -3,10 +3,28 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Hello ');
 });
-app.get('/add/:num1/:num2', function (req, res) {
+app.get('/dodaj/:num1/:num2', function (req, res) {
     var num1 = parseInt(req.params.num1);
     var num2 = parseInt(req.params.num2);
     var result = num1 + num2;
     res.send(num1 + ' + ' + num2 + ' = ' + result);
+});
+app.get('/usun/:num1/:num2', function (req, res) {
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    var result = num1 - num2;
+    res.send(num1 + ' - ' + num2 + ' = ' + result);
+});
+app.get('/podziel/:num1/:num2', function (req, res) {
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    var result = num1 / num2;
+    res.send(num1 + ' / ' + num2 + ' = ' + result);
+});
+app.get('/pomnoz/:num1/:num2', function (req, res) {
+    var num1 = parseInt(req.params.num1);
+    var num2 = parseInt(req.params.num2);
+    var result = num1 * num2;
+    res.send(num1 + ' * ' + num2 + ' = ' + result);
 });
 app.listen(3000);
